cd89a0ea1f7b656032ba11b0714a00eebe90b2ac
Radek Burget <radkovo@users.sf.net>
2012-12-20 08:54:02 +0000

    Width recomputation loop fix (provisional)

--- src/org/fit/cssbox/swingbox/view/ViewportView.java
+++ src/org/fit/cssbox/swingbox/view/ViewportView.java
@@ -215,7 +215,7 @@ public class ViewportView extends BlockBoxView implements ComponentListener
     {
         if ((e.getSource() instanceof JViewport))
         {
-            checkSize(((JViewport) e.getSource()).getExtentSize());
+            checkSize(((JViewport) e.getSource()).getSize());
         }
     }
 
@@ -239,11 +239,10 @@ public class ViewportView extends BlockBoxView implements ComponentListener
         if (extentSize.width == 0 || extentSize.height == 0)
             return false;
         
-        int diffx = Math.abs(extentSize.width - tmpDimension.width);
-        int diffy = Math.abs(extentSize.height - tmpDimension.height);
-        System.out.println("Diffx="+diffx);
+        int diffx = Math.abs(extentSize.width - box.getViewport().getBounds().width);
+        int diffy = Math.abs(extentSize.height - box.getViewport().getBounds().height);
         if (diffx > 20 || diffy > 20) //prevent loops caused by displaying scrollbars and too small changes.
-        {
+        {                             //TODO this is a provisional solution
             Document doc = getDocument();
             tmpDimension.setSize(extentSize);
 
